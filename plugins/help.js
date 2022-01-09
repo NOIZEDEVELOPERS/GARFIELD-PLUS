1694b5bd64e1f165c99e34118ea84b2eb7b34ff0
ＮＯＩＺ Ξ <86784191+Zenoixnoize@users.noreply.github.com>
2022-01-09 12:33:39 +0530

    Add files via upload

---
+++ plugins/help.js
@@ -0,0 +1,324 @@
+/* Codded by @phaticusthiccy
+Telegram: t.me/phaticusthiccy 
+Instagram: www.instagram.com/kyrie.baran
+*/
+
+const MyPnky = require('../events');
+const {MessageType, MessageOptions} = require('@adiwajshing/baileys');
+const Config = require('../config');
+
+// ==================== MAIN DESCRIPTION TEXT ====================
+const h_Des = "Yardım menüsünden botu kullanım hakkında bilgi verir."
+const h_DedEN = "Gives information about using the bot from the Help menu."
+const matchnullEN = "*🧚‍♂️ ＧＡＲＦＩＥＬＤ 🧚‍♂️*
+
+*🦄.xmedia*  
+🌸  It is a plugin with more than 25 media tools ⚾🌻️.
+
+
+*🦄.anime*   
+🌸 random anime image 🦋.
+
+*🦄.apkmod*  
+ 🌸 gives  mod apks 🎮.
+
+*🦄.ttp*     
+🌸 Converts text to plain painting 🌻️ .
+
+*🦄.attp*    
+⛄
+🌸 Adds rainbow effect to the text as a sticker 🏳️‍🌈 .
+
+
+*🦄.carbon*  
+ 🌸 It sends carbon picture 🐰.
+
+
+*🦄.mp3*     
+🌸 Converts video to sound 🔊.
+
+*🦄.photo*   
+🌸 Converts the sticker to a photo 📺.
+
+*🦄.mp4*     
+⛄ Converts animated stickers to video 🎞️.
+
+*🦄.ffpack*  
+🌸 Free Fire logo maker 🎮 
+
+*🦄.ffpack*  
+🌸 Free Fire logo maker 🐼
+
+
+*🦄.moretxt* 
+🌸 more txtit commands 👥
+
+*🦄.ffire*   
+🌸 add your text to random freefire logo 🎮 
+
+*🦄.emo*     
+🌸 emoji to png 🥶
+
+*🦄.notes*   
+🌸 Shows all your existing notes. 📖 
+
+*🦄.save*    
+🌸 Reply a message and type .save or just use .save <Your note> without replying 📕 
+
+
+*🦄.jid*     
+🌸 Giving user's JID 🆔
+
+*🦄.random*  
+🌸 word image 🌻️
+
+*🦄.brdmore* 
+🌸 add readmore before your text 🐧
+
+*🦄.rdmore*  
+🌸 add readmore before your text 🐥
+
+*🦄.removebg*
+🌸 Removes the background of the photos. 🏔️
+
+*🦄.report*  
+🌸 Sends reports to group admins 🚫 .
+
+*🦄.scan*    
+🌸 Checks whether the entered number is registered on WhatApp 🧬.
+
+*🦄.trt*     
+⛄ It translates with Google Translate. You must reply any message ⚙️.
+
+*🦄.currency 💰* 
+
+*🦄.tts*     
+🌸 It converts text to sound 🎙️.
+ 🌻️ Ex: .tts {si} Text 
+
+*🦄.song*    
+🌸 Uploads the song you wrote 🎼 .
+
+*🦄.video*   
+🌸 Downloads video from YouTube 🎞️.
+
+*🦄.sing*    
+🌸 It sings song that you have written 🌻️
+
+*🦄.isong*   
+🌸 Uploads the song you wrote for ios users 🎶 .
+
+*🦄.wiki*    
+🌸 Searches query on Wikipedia 📚.
+ 🌻️ Ex: .wiki sri lanka 🇱🇰 
+
+*🦄.img*     
+🌸 Searches for related pics on Google🏖️.
+
+*🦄.github*  
+🌸 Collects github information from the given username 🗼.
+🌻️ Example: .github zenoixnoize 
+
+*🦄.lyric 📝*   
+🌸 Finds the lyrics of the song.
+
+*🦄.covid*   
+🌸 Shows the daily and overall covid table of more than 15 countries 🧬.
+
+*🦄.sweather*
+🌸 Gives you the weekly interpretations of space weather observations provided by the Space Weather Research Center (SWRC) 🌡️.
+
+*🦄.hmod*    
+🌸 Finds mod apps from happymod 🤿
+
+
+*🦄.movie*   
+🌸 Shows movie info 🎬. 
+
+*🦄.joke*    
+🌸 It sends random jokes 🃏 
+
+*🦄.quote*   
+🌸 It shares famous quotes 💡
+
+*🦄.ss*      
+🌸 Takes a screenshot from the page in the given link 📑 .
+
+
+*🦄.show*    
+🌸 Get info related to tv series and shows 📺
+
+*🦄.ig*     
+🌸 Fetches user informations from instagram 🗽
+🌻️ Ex: .ig liyanage_tharindu_
+
+*🦄.animesay*
+🌸 It writes the text inside the banner the anime girl is holding 🤶
+
+*🦄.trumpsay*
+🌸 Converts the text to Trump's tweet 🇦🇨.
+
+*🦄.spdf*    
+🌸 Converts Site to PDF 🗞️.
+
+*🦄.sticker*
+🌸 It converts your replied photo or video to sticker 🧥.
+
+*🦄.tagadmin*
+🌸 Tags group admins 🥇.
+
+*🦄.forward* 
+🌸 Converts audio to sound recording 🎙️.
+
+*🦄.unvoice* 
+🌸 Converts audio to sound recording 📞.
+
+*🦄.wame*    
+🌸 Get a link to the user chat 📧.
+
+*🦄.weather*
+🌸 Shows the weather 🌻️ .
+
+*🦄.owner*
+🌸 Developer Contact and details 🐤*
+
+*🦄.ping*  
+🌸 Measures your ping ☘️.
+
+*🦄.short* 
+🌸 Shorten the long link 🌻️.
+
+*🦄.calc*    
+🌸 Performs simple math operations 💱. 
+
+*🦄.whois*  
+🌸 Displays metadata data of group or person 👽 .
+
+*🌻️🌻️🌻️🌻️🌻️🌻️🌻️🌻️🌻️*
+
+*🐼  🅶🅰︎🆁🅵🅸🅴🅻🅳 𝗗Ξ𝗩Ξ𝗟𝗢𝗣Ξ𝗥▱▱ 🐼*
+
+*🌻️ Facebook NOIZE  https://m.facebook.com/NOIZEHACKERS ZENOI*
+*🌻️ Instagram https://www.instagram.com/ze_noize?r=nametag*  
+*🌻️ Tharindu Liyanage Instagram https://www.instagram.com/liyanage_tharindu_?r=nametag,*
+*🌻️ Telegram https://t.me/TharinduLiyanage_bot*
+*🌻️ Developer facebook - https://www.facebook.com/zenoixnoize*
+*🌻️ HOSTING 🐼 -*
+ *Vertual Private Server by  Tharindu Liyanage 🇸🇬*
+ *𝘖𝘱𝘦𝘯 𝘚𝘰𝘶𝘳𝘤𝘦 𝘗𝘳𝘰𝘫𝘦𝘤𝘵*
+ *𝘈𝘭𝘭 𝘙𝘪𝘨𝘩𝘵𝘴 𝘙𝘦𝘴𝘦𝘳𝘷𝘦𝘥 ©️*
+
+*🦄🌻𝖬Λ𝖣Ξ 𝖨𝖭 𝖲𝖱𝖨 𝖫Λ𝖭𝖪Λ 🇱🇰*"
+const matchnull = "========== *🆘 Genel Yardım 🆘* ==========\n\n🔹 *.alive:* Botun çalışıp çalışmadığını kontrol eder.\n🔹 *.asena:* Tüm komut listesini gösterir.\n🔹 *.setvar:* Herokuya girmeden config ayarlar.\n\n🔸 Daha fazla yardım için ```.help <yardım almak istediğiniz konu>``` komutunu kullanın.\nÖrnek: ```.help botumu nasıl public yaparım?```\n\n========== *Genel Yardım Bitti* =========="
+const notfound = "```Almak istediğiniz yardım bulunamadı!```\n```Lütfen daha açıklayıcı bir şekilde sorunu belirtin.```"
+const notfoundEN = "```The help you wanted to get was not found!```\n```Please state the problem in a more descriptive way.```"
+
+// ==================== ALL DESCRİPTİONS ====================
+const pubTR = "Botunuzu public yapmak komutları herkese açık yapacaktır. Public yapıldıktan sonra kullanıcı sadece kişisel ve admin komutlarını kullanabilir hale gelir. Onun dışında komutları kullanamaz.\nBotunuzu public yapmak için *.setvar WORK_TYPE:public* komutunu kullanın."
+const pubEN = "Making your bot public will make the commands public. After it is made public, the user can only use personal and admin commands. User cannot use commands other than this.\n To make your bot public, type *.setvar WORK_TYPE:public*"
+const blTR = "BlockChat botunu belirlediğiniz gruba, kişiye veya birden fazla sohbete kapatır.\nKullanmak için önce sohbete gidip *.jid* yazın. Daha sonra gelen *90xxx-xxx@g.us veya 90xxx@whatsapp.net* yazısını kopyalayın. (@g.us ve @whatsapp.net'i silin!)\nArdından *.setvar BLOCK_CHAT:id && id1,id2...* komutunu kullanın."
+const sudoTR = "SUDO, botunuzu seçtiğiniz kullanıcıya tüm yetkileri ile paylaşır. Numara sonuna ,0 koyarsanız gruplarda da aktif hale gelir.\nKullanmak için *.setvar SUDO:90xxxx && 90xx,90xxx [ülke kodu ile, (❌ +90xx • ✅ 90xx)]"
+
+const privTR = "Botunuzu private yapmak komutları sadece size özel yapar. Başkaları kullanamaz.\nBotunuzu private yapmak için *.setvar WORK_TYPE:private* komutunu kullanın."
+const privEN = "Making your bot private makes commands private only for you. Anyone cannot use.\nTo make your bot private, type *.setvar WORK_TYPE:private*"
+const blEN = "Closes BlockChat bot to group, person or multiple chats you specify.\n To use it, first go to chat and type *.jid* Then copy to incoming code. (Except @g.us or @whatsapp.net!)\nThen use this command *.setvar BLOCK_CHAT:id && id1,id2..*"
+const sudoEN = "SUDO, Shares your bot to the user you choose with all its powers.If you put ,0 at the end of the number, the user can also use it in the group.\nTo use, type *.setvar SUDO:90xxxx && 90xx,90xxx [with county code, (❌ +90xx • ✅ 90xx)]*"
+
+if (Config.STANDPLK == 'off' || Config.STANDPLK == 'OFF') {
+if (Config.LANG == 'TR' || Config.LANG == 'AZ') {
+    
+    MyPnky.addCommand({pattern: 'help ?(.*)', fromMe: true, desc: h_Des}, (async (message, match) => {
+
+        if (match[1] === '') {
+            return await message.client.sendMessage(
+                message.jid,
+                matchnull,
+                MessageType.text
+            );
+        }
+        else if ( (match[1].includes('public') && match[1].includes('nasıl')) || (match[1].includes('public') && match[1].includes('yapimi')) || (match[1].includes('public') && match[1].includes('yapımı')) || (match[1].includes('public') && match[1].includes('nedir')) || (match[1].includes('herkese') && match[1].includes('acik') || match[1].includes('açık')) ) {
+            return await message.client.sendMessage(
+                message.jid,
+                pubTR,
+                MessageType.text
+            );
+        }
+        else if ( (match[1].includes('private') && match[1].includes('nasıl')) || (match[1].includes('private') && match[1].includes('yapimi')) || (match[1].includes('private') && match[1].includes('yapımı')) || (match[1].includes('private') && match[1].includes('nedir')) || (match[1].includes('bana') && match[1].includes('özel')) ) {
+            return await message.client.sendMessage(
+                message.jid,
+                privTR,
+                MessageType.text
+            );
+        }
+        else if ( (match[1].includes('belirli') && match[1].includes('sohbet')) || (match[1].includes('sohbet') && match[1].includes('kapat')) || (match[1].includes('nasil') && match[1].includes('kapatabilirim')) || (match[1].includes('blockchat') && match[1].includes('nedir')) || (match[1].includes('sohbet') && match[1].includes('özel')) ) {
+            return await message.client.sendMessage(
+                message.jid,
+                blTR,
+                MessageType.text
+            );
+        }
+        else if ( (match[1].includes('SUDO') && match[1].includes('nedir')) || (match[1].includes('SUDO') && match[1].includes('nasil')) || (match[1].includes('botu') && match[1].includes('baskasina')) || (match[1].includes('botu') && match[1].includes('arkadaşıma')) || (match[1].includes('SUDO') && match[1].includes('kullanmak')) ) {
+            return await message.client.sendMessage(
+                message.jid,
+                sudoTR,
+                MessageType.text
+            );
+        }
+        else {
+            return await message.client.sendMessage(
+                message.jid,
+                notfound,
+                MessageType.text
+            );
+        }
+    }));
+}
+else {
+    
+    MyPnky.addCommand({pattern: 'help ?(.*)', fromMe: true, desc: h_DedEN}, (async (message, match) => {
+
+        if (match[1] === '') {
+            return await message.client.sendMessage(
+                message.jid,
+                matchnullEN,
+                MessageType.text
+            );
+        }
+        else if ( (match[1].includes('public') && match[1].includes('how')) || (match[1].includes('public') && match[1].includes('set')) || (match[1].includes('public') && match[1].includes('setting')) ) {
+            return await message.client.sendMessage(
+                message.jid,
+                pubEN,
+                MessageType.text
+            );
+        }
+        else if ( (match[1].includes('private') && match[1].includes('how')) || (match[1].includes('private') && match[1].includes('set')) || (match[1].includes('private') && match[1].includes('setting')) ) {
+            return await message.client.sendMessage(
+                message.jid,
+                privEN,
+                MessageType.text
+            );
+        }
+        else if ( (match[1].includes('SUDO') && match[1].includes('usage')) || (match[1].includes('SUDO') && match[1].includes('what')) || (match[1].includes('how') && match[1].includes('SUDO')) || (match[1].includes('set') && match[1].includes('SUDO')) || (match[1].includes('share') && match[1].includes('bot')) ) {
+            return await message.client.sendMessage(
+                message.jid,
+                sudoEN,
+                MessageType.text
+            );
+        }
+        else if ( (match[1].includes('block') && match[1].includes('chat')) || (match[1].includes('how') && match[1].includes('block')) || (match[1].includes('close') && match[1].includes('bot')) || (match[1].includes('specific') && match[1].includes('chat')) || (match[1].includes('specific') && match[1].includes('set')) ) {
+            return await message.client.sendMessage(
+                message.jid,
+                blEN,
+                MessageType.text
+            );
+        }
+        else {
+            return await message.client.sendMessage(
+                message.jid,
+                notfoundEN,
+                MessageType.text
+            );
+        }
+    }));
+}
+}
